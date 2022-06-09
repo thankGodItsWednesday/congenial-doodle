87072b1dd98e1f6f41107fcdd79e83ec50134e5e
thankGodItsWednesday <jshsarada@gmail.com>
2022-06-09 11:32:07 -0700

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
   
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
